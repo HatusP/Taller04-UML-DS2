49be89136a90d7044a3cb109d53e99ed6ff8482c
User <hatuspellegrini1@gmail.com>
2020-11-24 18:46:34 -0500

    Añadiendo Clases Cita, Paciente e interfaz Pago

---
+++ Taller04-UML/src/taller05/uml/Cita.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller05.uml;
+
+import java.util.Date;
+import taller05.uml.Pagos.Pago;
+
+/**
+ *
+ * @author User
+ */
+public class Cita {
+    protected Date fecha;
+    protected boolean pagada;
+    protected String registradoPor;
+    public Paciente paciente;
+    
+    public void realizarPago(Pago pago){
+        
+    }
+    
+}
---
+++ Taller04-UML/src/taller05/uml/Paciente.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller05.uml;
+
+/**
+ *
+ * @author User
+ */
+public class Paciente {
+    protected String email;
+    public Cita cita;
+    
+    public boolean solicitarCita(){
+        return false;
+        
+    }
+}
