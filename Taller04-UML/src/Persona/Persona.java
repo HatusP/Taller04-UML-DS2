2ca4c4af2b50cf60577a0925ddb176d92b848b0b
NahinMontoya <73256114+NahinMontoya@users.noreply.github.com>
2020-11-24 18:54:36 -0500

    Se creo la clase Persona

---
+++ Taller04-UML/src/Persona/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persona;
+
+import java.util.Date;
+
+
+public class Persona {
+       protected String usuario;
+   protected String clave;
+   protected String nombre;
+   protected String apellido;      
+   protected String cedula;      
+   protected String dirrecion;      
+   protected Date fechaNac ;        
+           
+public boolean logln( ){return true;
+}
+public boolean logOut( ){return true;
+}
+}
